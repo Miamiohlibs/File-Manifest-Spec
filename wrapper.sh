d3f384da8ebd39cab5bf0ea763e7f619512bd02e
Ken Irwin <ken@kenirwin.net>
2025-03-19 10:11:23 -0400

    use header and data flags in wrapper for cleaner output

--- wrapper.sh
+++ wrapper.sh
@@ -17,13 +17,15 @@ fi
 
 all_dirs=$(ls -d "$dir"/*/)
 
+#first, print the header only
+./create-manifest.sh -H "$dir"
+
 while IFS= read -r subdir; do
     folder_name=$(basename "$subdir")
     if [ "$folder_name" = "#recycle" ]; then
-        #ignore
-        echo ""
+        : # do nothing
     else
         # echo "$folder_name"
-        ./create-manifest.sh "$subdir"
+        ./create-manifest.sh -d "$subdir"
     fi
 done <<< "$all_dirs"
